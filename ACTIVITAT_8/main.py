39e7c581ef95d0343c48e09590bf49efd44d9273
alexrss85 <2023_alex.rosendo@iticbcn.cat>
2024-11-13 20:32:59 +0100

    activitat punt 7

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -8,8 +9,11 @@ app = FastAPI()
 async def root():
     return {"1":"Alex","2":"Arnau","3":"Adri","4":"Sergi"}
 
-@app.get("/contactes/{contacte_id}", status_code=404)
+contactes = {"1":"Alex","2":"Arnau","3":"Adri","4":"Sergi"}
+@app.get("/contactes/{contacte_id}")
 async def read_item(contacte_id):
+    if contacte_id not in contactes:
+        raise HTTPException(status_code=404, detail="Contacte inexistent amb aquesta ID")
     return {"contacte_id": contacte_id}
 
 
--- README.md
+++ README.md
@@ -6,3 +6,20 @@ GET:
     ![alt text](./ACTIVITAT_8/img/getSwagger.png)
     POSTMAN
     ![alt text](./ACTIVITAT_8/img/getPostman.png)
+
+POST: 
+    SWAGGER
+    ![alt text](ACTIVITAT_8/img/postSwagger.png)
+    POSTMAN
+    ![alt text](ACTIVITAT_8/img/postPostman.png)
+
+RESPONSE
+    SWAGGER
+    ![alt text](ACTIVITAT_8/img/errorGetSwagger.png)
+    POSTMAN
+    ![alt text](ACTIVITAT_8/img/errorGet.png)
+HTTPException
+    SWAGGER
+    ![alt text](ACTIVITAT_8/img/errorHTTPswagger.png)
+    POSTMAN
+    ![alt text](ACTIVITAT_8/img/errorHTTPpostman.png)
