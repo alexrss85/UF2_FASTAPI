bc1d819dba6920877fefbe14d49521deb6302c56
alexrss85 <2023_alex.rosendo@iticbcn.cat>
2024-11-13 20:19:21 +0100

    activitat punt 6

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
 @app.get("/contactes")
 async def root():
-    return {"1": "Alex","2":"Adri","3":"Arnau","4":"Sergi"}
+    return {"1":"Alex","2":"Arnau","3":"Adri","4":"Sergi"}
+
+@app.get("/contactes/{contacte_id}", status_code=404)
+async def read_item(contacte_id):
+    return {"contacte_id": contacte_id}
+
+
+class Item(BaseModel):
+    nom: str
+    cognom: str
+    edat: int
+    id: int
+    curs: str
+    correu: float | None = None
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
