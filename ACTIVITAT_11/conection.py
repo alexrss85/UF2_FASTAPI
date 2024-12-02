7cd7d8857408400aa117f92f8083201bb1a00d6c
alexrss85 <2023_alex.rosendo@iticbcn.cat>
2024-12-02 19:12:08 +0100

    classe dilluns

---
+++ ACTIVITAT_11/conection.py
@@ -0,0 +1,18 @@
+import psycopg2 as psy
+
+#conexió a la db
+def conexio():
+    try:
+            conn = psy.connect(
+                database="postgres",
+                user="user_postgres",
+                password="pass_postgres",
+                host="localhost",
+                port="5432"
+            )
+            return conn
+
+    except (Exception, psy.Error) as error:
+        print("Error",error)
+        return(None,None)
+    
